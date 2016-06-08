43ad0f6c3c3f3c2ddd243bc2923b69a2d65e54ba
Aleksandr Grinevich <a.b.grinevich@gmail.com>
2016-06-08 16:40:57 +0500

    Добавлена админка

--- lib/form/JobeetJobForm.class.php
+++ lib/form/JobeetJobForm.class.php
@@ -11,6 +11,13 @@ class JobeetJobForm extends BaseJobeetJobForm
 {
   public function configure()
   {
+      $this->removeFields();
+      
+      $this->validatorSchema['email'] = new sfValidatorAnd(array(
+      $this->validatorSchema['email'],
+      new sfValidatorEmail(),
+      ));
+      
       $this->useFields(array('category_id', 'type', 'company', 'logo', 'url', 
           'position', 'location', 'description', 'how_to_apply', 'is_public', 'email'));
       
@@ -46,5 +53,14 @@ class JobeetJobForm extends BaseJobeetJobForm
       $this->widgetSchema->setHelp('is_public', 'Whether the job can also be published on affiliate websites or not.');
       
       $this->widgetSchema->setNameFormat('job[%s]');
+  }
+  
+  protected function removeFields()
+  {
+    unset(
+      $this['created_at'], $this['updated_at'],
+      $this['expires_at'], $this['is_activated'],
+      $this['token']
+    );
   }
 }
